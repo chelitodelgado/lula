33469ce39e816231f2cf2e20482628a164fb142c
Angel Paredes Torres <30500596+chelitodelgado@users.noreply.github.com>
2021-09-08 18:27:06 -0500

    Update parameter.php

--- config/parameter.php
+++ config/parameter.php
@@ -1,6 +1,6 @@
 <?php
 
-define("base_url", "http://localhost:8080/php/fwk_lula/");
+define("base_url", "RUTA DEL PROYECTO O DEL SERVIDOR");
 define("controler_default", "homeController");
 define("title", "Titulo de la pagina");
 define("name", "Este es un framawork base para realizar proyectos web de uso libre.");
